8530b9c064d1ae2c1db000ecb5af21bd65ee206d
Willian Rodrigo <will87padua@gmail.com>
2021-10-21 16:17:10 -0300

    Template add

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require ("@rails/ujs").start()
+require ("turbolinks").start()
+require ("@rails/activestorage").start()
+require ("channels")
+//require ("packs/custom")
+//require ("packs/posts.js")
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+import './src/application.scss'
 
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+  
+  
+
